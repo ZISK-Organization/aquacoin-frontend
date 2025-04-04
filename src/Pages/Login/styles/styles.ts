7db33918a2a34309e87078b54e8c8178cf1b3426
StastnyJ <xstastnyj@gmail.com>
2025-04-04 09:58:33 +0200

    ~ fix responsive login

--- src/Pages/Login/styles/styles.ts
+++ src/Pages/Login/styles/styles.ts
@@ -18,7 +18,7 @@ const stylesDefinition: StylesDefinition = {
         default: {
             border: "1px solid lightgray",
             borderRadius: "16px",
-            width: 462,
+            width: `max(462px, 90%)`,
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
